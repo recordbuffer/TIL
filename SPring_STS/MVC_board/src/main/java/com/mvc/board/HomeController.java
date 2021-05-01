012d8d36af007fd5c1ac62901e383809daec6fc0
recordbuffer <thal96@kyonggi.ac.kr>
2021-05-01 22:18:11 +0900

    board

--- SPring_STS/MVC_board/src/main/java/com/mvc/board/HomeController.java
+++ SPring_STS/MVC_board/src/main/java/com/mvc/board/HomeController.java
@@ -1,17 +1,11 @@
 package com.mvc.board;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.mvc.board.model.biz.BoardBiz;
 import com.mvc.board.model.dto.BoardDto;
@@ -27,28 +21,16 @@ public class HomeController {
 	private BoardBiz biz;
 	
 	
-	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
 	@RequestMapping("/list.do")
-	public String list(Model model) {
-		logger.info("SELECT LIST");
+	public String main(Model model) {
+		logger.info("MAIN PAGE");
 		
 		model.addAttribute("list",biz.selectList());
 		
 		return "main";
 	}
+	
+
 	
 	@RequestMapping("/detail.do")
 	public String detail(Model model, int myno) {
