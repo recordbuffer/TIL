faee645cc9f97a70ff3ad83e620c2993ae313018
recordbuffer <thal96@kyonggi.ac.kr>
2021-05-17 23:26:55 +0900

    kakao book

---
+++ Servlet/KBook_API/src/com/book/controller/bookServlet.java
@@ -0,0 +1,33 @@
+package com.book.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/book.do")
+public class bookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 인코딩
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+
+		// 요청받을 변수 선언
+		String command = request.getParameter("command");
+		
+		if(command.equals("searchpage")) {
+			response.sendRedirect("searchpage.jsp");
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Servlet/KBook_API/webcontent/searchpage.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>도서 검색</h1>
+		<input type="text" name="query">
+		<input type="button" value="검색" onclick="search();">
+	
+	<div></div>
+</body>
+</html>
