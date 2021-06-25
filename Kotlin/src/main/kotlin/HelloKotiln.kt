5661d32a0b8ac01490e785c92cb1e351561c3f81
recordbuffer <thal96@kyonggi.ac.kr>
2021-06-25 21:59:49 +0900

    hello_kotiln

--- Kotlin/src/main/kotlin/HelloKotiln.kt
+++ Kotlin/src/main/kotlin/HelloKotiln.kt
@@ -1,5 +1,16 @@
-class HelloKotiln {
-}
 fun main(){
     println("Hello, World")
+}
+fun main(args: Array<String>) {
+    //args (인자) 호출하기 위해선 미리 넣어줘야함
+    //1. Edit configuration
+    //2. Program arguments 에 넣기 Ok
+    //3. 실행...
+    println(args[1])
+    println(args[2])
+    println(args[3])
+
+    //4. 변수 표현 실행...
+    println("args[0] = ${args[0]}")
+
 }
