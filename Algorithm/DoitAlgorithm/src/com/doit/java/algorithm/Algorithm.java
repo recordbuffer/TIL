e0661f762fd34a32e19262baa034cac7a234893f
recordbuffer <thal96@kyonggi.ac.kr>
2021-07-07 23:57:03 +0900

    do_it 알고리즘

---
+++ Algorithm/DoitAlgorithm/src/com/doit/java/algorithm/Algorithm.java
@@ -0,0 +1,68 @@
+package com.doit.java.algorithm;
+
+import java.util.Scanner;
+
+public class Algorithm {
+
+	public static void main(String[] args) {
+		// 알고리즘 : 문제해결을 위한 단계적인 절차
+		
+		//Algorithm.max();
+		//Algorithm.center();
+		Algorithm.operation();
+	}
+	
+	public static void max() {
+	// 정수 3개 입력받아 최댓값 출력
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+				
+		int max = a;
+				
+		if(max<b) max=b;
+		if(max<c) max=c;
+				
+		System.out.println("최댓값은 "+max+"이다");
+	}
+	
+	public static void center() {
+	// 정수 3개 입력받아 중앙값 출력
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		
+		int center = a;
+		
+		if(a>=b)
+			if(b>=c) 
+				center = b;
+			else if(a<=c)
+				center = a;
+			else
+				center = c;
+		else if(a>c)
+			center = a;
+		else if(b>c)
+			center = c;
+		else
+			center = b;
+			
+		System.out.println("중앙값은 "+center+"이다");
+	}
+	
+	public static void operation() {
+	// 입력한 정수 양수인지 음수인지 0인지
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		if(n>0) System.out.println("양수입니다.");
+		if(n<0) System.out.println("음수입니다.");
+		else
+			System.out.println("0입니다");
+		
+	}
+}
---
+++ Algorithm/DoitAlgorithm/src/com/doit/java/algorithm/Loop.java
@@ -0,0 +1,45 @@
+package com.doit.java.algorithm;
+
+import java.util.Scanner;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+		//Loop.whilesum();
+		//Loop.forsum();
+	}
+	
+	public static void whilesum() {
+	// 1부터 입력받은 n 까지 합
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int sum = 0;
+		int i = 1;
+		
+		while(i<=n) {
+			sum += i;
+			i++;
+		}
+		System.out.println("1부터 "+n+"까지의 합계 : "+sum);
+	}
+	
+	
+	public static void forsum() {
+	// 1부터 입력받은 n 까지 합
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int sum = 0;
+		
+		for(int i=1; i<=n; i++) {
+			sum += i;
+		}
+		System.out.println("1부터 "+n+"까지의 합계 : "+sum);
+	}
+	
+	
+	
+	
+}
