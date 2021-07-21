78ea20a7b0b5f1334721f88553259ef88bccf049
recordbuffer <thal96@kyonggi.ac.kr>
2021-07-21 22:30:12 +0900

    chap1 완료 + chap2 시작

--- Algorithm/DoitAlgorithm/src/com/doit/java/algorithm/Structured.java
+++ Algorithm/DoitAlgorithm/src/com/doit/java/algorithm/Structured.java
@@ -6,7 +6,9 @@ public class Structured {
 
 	public static void main(String[] args) {
 		
-		Structured.digits();
+		//Structured.digits();
+		//Structured.multiLoop();
+		Structured.triangle();
 	}
 	
 	public static void digits() {
@@ -19,9 +21,42 @@ public class Structured {
 		do {
 			no = sc.nextInt();
 			
-		} while(no< 10 || no>99);
+		} while(!(no>=10 && no<=99));
+		//while(no<10 || no>99);
 		
 		System.out.println("변수 no의 값은   "+no+" 입니다");
 	}
-
+	
+	
+	public static void multiLoop() {
+	//곱셈표 출력
+		System.out.println("-----곱셈표 출력-----");
+		
+		for(int i=1; i<=9; i++) {
+			for(int j=1; j<=9; j++) {
+				System.out.printf("%3d",i*j);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void triangle() {
+	//이등변 삼각형 출력
+		Scanner sc = new Scanner(System.in);
+		System.out.println("이등변 삼각형의 단수 입력");
+		
+		int no;
+		
+		do {
+			no = sc.nextInt();
+			
+		}while(no<=0);
+		
+		for(int i=1; i<=no; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
 }
---
+++ Algorithm/DoitAlgorithm/src/com/doit/java/dataStructure/Array.java
@@ -0,0 +1,40 @@
+package com.doit.java.dataStructure;
+
+public class Array {
+
+	public static void main(String[] args) {
+		//Array.intArray();
+		Array.cloneArray();
+	}
+
+	public static void intArray() {
+	//구성요소 5개짜리 int배열 출력
+		int[] a = new int[5];
+		
+		a[1]=37;
+		a[2]=51;
+		a[4]=a[1]*2;
+		
+		for(int i=0; i<a.length; i++) {
+			System.out.println("a["+i+"] ="+a[i]);
+		}
+	}
+	
+	public static void cloneArray() {
+	//배열 복제 출력
+		int[] a = {1,2,3,4,5};
+		int[] b = a.clone();
+		
+		b[3] = 0;
+		
+		System.out.print("a =");
+		for(int i=0; i<a.length; i++) {
+			System.out.print(" "+a[i]);
+		}
+		
+		System.out.print("\nb =");
+		for(int j=0; j<b.length; j++) {
+			System.out.print(" "+b[j]);
+		}
+	}
+}
