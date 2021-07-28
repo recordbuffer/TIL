1f8728766851671e64af3c275e3a678f9ab880cb
recordbuffer <thal96@kyonggi.ac.kr>
2021-07-28 23:31:37 +0900

    단위test

--- Spring_Boot/hello-springBoard/src/test/java/helloBoard/hellospringBoard/domain/posts/PostsRepositoryTest.java
+++ Spring_Boot/hello-springBoard/src/test/java/helloBoard/hellospringBoard/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,46 @@
 package helloBoard.hellospringBoard.domain.posts;
 
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class PostsRepositoryTest {
 
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After                     //단위 테스트 끝날때마다 실행될 메소드
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+        .title(title)
+        .content(content)
+        .author("springboot@mail.com")
+        .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
 }
