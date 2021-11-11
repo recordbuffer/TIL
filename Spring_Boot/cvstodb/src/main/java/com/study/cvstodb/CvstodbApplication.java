6fb00c018010e13ae77eb24283bff44e8d8400d0
recordbuffer <thal96@kyonggi.ac.kr>
2021-11-11 23:09:24 +0900

    cvs to db 실습

---
+++ Spring_Boot/cvstodb/src/main/java/com/study/cvstodb/CvstodbApplication.java
@@ -0,0 +1,13 @@
+package com.study.cvstodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvstodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvstodbApplication.class, args);
+	}
+
+}
---
+++ Spring_Boot/cvstodb/src/test/java/com/study/cvstodb/CvstodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.cvstodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvstodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
