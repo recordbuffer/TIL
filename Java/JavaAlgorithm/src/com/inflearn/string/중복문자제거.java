7a9b83e040439532259df245c9a8d07b06204430
recordbuffer <thal96@kyonggi.ac.kr>
2021-12-20 21:48:32 +0900

    자바 알고리즘 중보문자제거

---
+++ Java/JavaAlgorithm/src/com/inflearn/string/중복문자제거.java
@@ -0,0 +1,25 @@
+package com.inflearn.string;
+
+import java.util.Scanner;
+
+//소문자로 된 한개의 문자열이 입력되면 중복된 문자를 제거하고 출력하는 프로그램을 작성하세요.
+//중복이 제거된 문자열의 각 문자는 원래 문자열의 순서를 유지합니다.
+//입력: ksekkset
+//출력: kset
+public class 중복문자제거 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		
+		System.out.println(solution(s));
+	}
+	
+	public static String solution(String s) {
+		String res = "";
+		
+		
+		
+		return res;
+	}
+
+}
