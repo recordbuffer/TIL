cf6811674cf625b45f9d85afd4d587ea78a0b1ee
recordbuffer <76642597+recordbuffer@users.noreply.github.com>
2022-09-22 17:01:27 +0900

    Add cud by JDBC

---
+++ Spring_Boot/jpa_study/src/main/java/com/udemy/jpa_study/SpringJdbcApplication.java
@@ -0,0 +1,34 @@
+package com.udemy.jpa_study;
+
+import com.udemy.jpa_study.domain.Person;
+import com.udemy.jpa_study.jdbc.PersonJdbcDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class SpringJdbcApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJdbcDao dao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		/*logger.info("All users -> {}", dao.findAll());
+		logger.info("User id 10001 -> {}", dao.findById(10001));
+		logger.info("Deleting 10002 -> No of Rows Deleted - {}", dao.deleteById(10002));
+		logger.info("Inserting 10004 -> {}",dao.insert(new Person(10004, "Tara", "Berlin", new Date())));
+		logger.info("Update 10003 -> {}", dao.update(new Person(10003, "Pieter", "Utrecht", new Date())));*/
+	}
+}
--- Spring_Boot/jpa_study/src/main/java/com/udemy/jpa_study/jdbc/PersonJdbcDao.java
+++ Spring_Boot/jpa_study/src/main/java/com/udemy/jpa_study/jdbc/PersonJdbcDao.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -22,4 +23,21 @@ public class PersonJdbcDao {
         return jdbcTemplate.queryForObject("select * from person where id=?", new Object[] { id },
                 new BeanPropertyRowMapper<Person>(Person.class));
     }
+
+    public int deleteById(int id) {
+        return jdbcTemplate.update("delete from person where id=?", new Object[] { id });
+    }
+
+    public int insert(Person person) {
+        return jdbcTemplate.update("insert into person (id, name, location, birth_date) " + "values(?,  ?, ?, ?)",
+                new Object[] { person.getId(), person.getName(), person.getLocation(),
+                        new Timestamp(person.getBirthDate().getTime()) });
+    }
+
+    public int update(Person person) {
+        return jdbcTemplate.update("update person " + " set name = ?, location = ?, birth_date = ? " + " where id = ?",
+                new Object[]{person.getName(), person.getLocation(), new Timestamp(person.getBirthDate().getTime()), person.getId()});
+    }
+
 }
+
