c68f0d5feb3c441f9881663a129af9f0002ac282
recordbuffer <thal96@kyonggi.ac.kr>
2022-11-06 22:25:16 +0900

    Add Spring Data Rest

--- Spring_Boot/jpa_study/src/main/java/com/udemy/jpa_study/springdatajpa/repository/CourseRepository.java
+++ Spring_Boot/jpa_study/src/main/java/com/udemy/jpa_study/springdatajpa/repository/CourseRepository.java
@@ -3,13 +3,15 @@ package com.udemy.jpa_study.springdatajpa.repository;
 import com.udemy.jpa_study.springdatajpa.domain.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Repository
-@Transactional
+/*@Repository
+@Transactional*/
+@RepositoryRestResource(path = "courses")
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     List<Course> findByName(String name);
