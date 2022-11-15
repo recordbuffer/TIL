455868eb648e2fceeef787c017b25f57dda3872d
recordbuffer <76642597+recordbuffer@users.noreply.github.com>
2022-11-15 17:03:26 +0900

    Add gRPC Module

---
+++ gRPC/src/main/java/ecommerce/ProductInfoImpl.java
@@ -0,0 +1,35 @@
+package ecommerce;
+
+import io.grpc.Status;
+import io.grpc.StatusException;
+import io.grpc.stub.StreamObserver;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ProductInfoImpl extends ProductInfoGrpc.ProductInfoImplBase{
+
+    private Map productMap = new HashMap<String, ProductInfoOuterClass.Product>();
+    @Override
+    public void addProduct(ProductInfoOuterClass.Product request, StreamObserver<ProductInfoOuterClass.ProductID> responseObserver) {
+        UUID uuid = UUID.randomUUID();
+        String randomUUIDString = uuid.toString();
+        request = request.toBuilder().setId(randomUUIDString).build();
+        productMap.put(randomUUIDString, request);
+        ProductInfoOuterClass.ProductID id = ProductInfoOuterClass.ProductID.newBuilder().setValue(randomUUIDString).build();
+        responseObserver.onNext(id);        // 클라이언트에 응답
+        responseObserver.onCompleted();     // 클라이언트 호출 종료
+    }
+
+    @Override
+    public void getProduct(ProductInfoOuterClass.ProductID request, StreamObserver<ProductInfoOuterClass.Product> responseObserver) {
+        String id = request.getValue();
+        if (productMap.containsKey(id)) {
+            responseObserver.onNext((ProductInfoOuterClass.Product) productMap.get(id));
+            responseObserver.onCompleted();
+        } else {
+            responseObserver.onError(new StatusException(Status.NOT_FOUND));
+        }        
+    }
+}
---
+++ gRPC/src/test/java/com/google/gRPC/GRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.gRPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
