a0ca0b7f94bcf2315239a878a003b150da995d89
MEE <meedev009@gmail.com>
2023-05-25 23:19:07 +0900

    Add FastAPI init

---
+++ FastAPI/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from enum import Enum
+
+app = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item}")
+async def read_item(item):
+    return {"item": item}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
