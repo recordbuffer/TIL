110a21b4954ffb6c7238f143662102f7cdf1b7ff
MEE <meedev009@gmail.com>
2023-06-08 01:00:10 +0900

    Add FastAPI request param, body

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+
+from typing import Union, Annotated
+
 from enum import Enum
 
 app = FastAPI()
@@ -14,9 +18,9 @@ class ModelName(str, Enum):
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item}")
-async def read_item(item):
-    return {"item": item}
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item": item_id}
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -27,3 +31,56 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(min_length=3, max_length=10)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
